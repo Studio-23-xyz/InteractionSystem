0bbbb1695ffe8eb1e648fa833c0572e7f001666f
B-DeshiDev <ffalsoy@gmail.com>
2024-01-24 11:28:53 +0600

    Merge pull request #17 from Studio-23-xyz/dev
    Fix tilda sample path

---
+++ Runtime/Data/InteractionConditionResult.cs
@@ -0,0 +1,9 @@
+namespace Studio23.SS2.InteractionSystem.Data
+{
+    public enum InteractionConditionResult
+    {
+        Show,//should show the interactionPrompt normally
+        Disable,//cannot interact with it, but show a disabled prompt
+        Hide//won't even show a prompt
+    }
+}
