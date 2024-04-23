73bcc1eca7d8719e8741e130887f9a00486c369a
B-DeshiDev <ffalsoy@gmail.com>
2024-04-23 10:46:19 +0600

    Merge pull request #21 from Studio-23-xyz/remove--inputt-directly-from-asset
    Remove  inputt directly from asset

---
+++ Samples/Demo/Scripts/InputAssetDrivenInteractionInputHandler.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections.Generic;
+using Bdeshi.Helpers.Input;
+using Bdeshi.Helpers.Utility;
+using Bdeshi.Helpers;
+using Bdeshi.Helpers.Utility;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Studio23.SS2.InteractionSystem.Core
+{
+    public class InputAssetDrivenInteractionInputHandler : InteractionInputHandlerBase
+    {
+        [SerializeField] private InputActionAsset _inputActionMap;
+        // TODO: FOR gamepad this needs to be multiplied by time.deltatime
+
+        #region Input Action Ref
+
+        [SerializeField] private InputActionReference _inspectDragDeltaAction;
+        [SerializeField] private InputActionReference _inspectAction;
+        [SerializeField] private InputActionReference _inspectDragAction;
+        [SerializeField] private InputActionReference _interactCancelAction;
+        [SerializeField] private InputActionReference _toggleAction;
+        [SerializeField] private InputActionReference _pickupAction;
+        [SerializeField] private InputActionReference _inspectMoveAction;
+        [SerializeField] private InputActionReference _inspectZoomAction;
+        [SerializeField] private InputActionReference _inspectResetAction;
+        [SerializeField] private InputActionReference _debug1Action;
+
+        #endregion
+
+        public List<InputButtonSlot> Buttons ;
+
+        private void Awake()
+        {
+            Buttons = new List<InputButtonSlot>();
+        }
+
+        private void OnZoomCancelled(InputAction.CallbackContext obj)
+        {
+            _inspectionZoomInput = 0;
+        }
+
+        private void OnZoomPerformed(InputAction.CallbackContext obj)
+        {
+            _inspectionZoomInput = obj.ReadValue<float>();
+        }
+
+        private void OnMoveCancelled(InputAction.CallbackContext obj)
+        {
+            _inspectionMoveInput = Vector2.zero;
+        }
+
+        private void OnMovePerformed(InputAction.CallbackContext obj)
+        {
+            _inspectionMoveInput = obj.ReadValue<Vector2>();
+        }
+        
+        #region Binding Events
+        public void Bind(InputButtonSlot slot, InputActionReference action)
+        {
+            if(action == null)
+            {
+                Debug.LogWarning("action null for button " + slot.ButtonName);
+                return;
+            }
+            slot.Bind(action);
+            Buttons.Add(slot);
+        }
+        void OnEnable()
+        {
+            BindAll();
+        }
+
+        private void BindAll()
+        {
+            if (_inputActionMap == null)
+                return;
+
+            _inputActionMap.Enable();
+
+            Buttons.Clear();
+            Bind(InspectButton, _inspectAction);
+            Bind(ToggleButton, _toggleAction);
+            Bind(PickupButton, _pickupAction);
+            Bind(InspectionDragButton, _inspectDragAction);
+            Bind(InteractCancelButton, _interactCancelAction);
+            Bind(InspectResetButton, _inspectResetAction);
+            
+            _inspectMoveAction.action.performed += OnMovePerformed;
+            _inspectMoveAction.action.canceled += OnMoveCancelled;
+            _inspectZoomAction.action.performed += OnZoomPerformed;
+            _inspectZoomAction.action.canceled += OnZoomCancelled;
+            _inspectDragDeltaAction.action.performed += OnLookPerformed;
+            _inspectDragDeltaAction.action.canceled += OnLookCancelled;
+        }
+        void OnDisable()
+        {
+            UnBindAll();
+        }
+
+
+        private void UnBindAll()
+        {
+            if (_inputActionMap == null)
+                return;
+
+            if (Buttons != null)
+            {
+                foreach (var inputButtonSlot in Buttons)
+                {
+                    inputButtonSlot.UnBind();
+                }
+            }
+
+            _inspectMoveAction.action.performed -= OnMovePerformed;
+            _inspectMoveAction.action.canceled -= OnMoveCancelled;
+            _inspectDragDeltaAction.action.performed -= OnLookPerformed;
+            _inspectDragDeltaAction.action.canceled -= OnLookCancelled;
+            _inputActionMap.Disable();
+        }
+
+        private void OnLookCancelled(InputAction.CallbackContext obj)
+        {
+            _inspectDragDelta = Vector2.zero;
+        }
+
+        private void OnLookPerformed(InputAction.CallbackContext obj)
+        {
+            _inspectDragDelta = obj.ReadValue<Vector2>();
+        }
+        #endregion
+    }
+}
