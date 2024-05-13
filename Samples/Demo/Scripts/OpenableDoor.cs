7508544f448ad0efefc105f8de042a8f364465ee
B-DeshiDev <ffalsoy@gmail.com>
2024-05-13 17:31:31 +0600

    Update package.json

---
+++ Samples/Demo/Scripts/OpenableDoor.cs
@@ -0,0 +1,64 @@
+using System.Threading;
+using Studio23.SS2.InteractionSystem.Abstract;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Studio23.SS2.InteractionSystem.Samples.Demo1
+{
+    public class OpenableDoor: OpenableBase
+    {
+        public float DoorAnimTime = 1.2f;
+        public GameObject DoorObject;
+        private Vector3 _closedEulerAngles;
+        private Vector3 _openedEulerAngles;
+        [SerializeField] private float _doorOpenAngle = 90;
+        [SerializeField] private float _disabledDoorPunchAmount = .0315f;
+        [SerializeField] private Sprite _markerIcon;
+        public override Sprite MarkerIcon => _markerIcon;
+        protected override async UniTask DoOpenInteraction(CancellationToken token)
+        {
+            await AnimateDoor(_openedEulerAngles)
+                .WithCancellation(token);
+        }
+        
+        Tween AnimateDoor(Vector3 endAngles)
+        {
+            return DoorObject.transform.DOLocalRotate(endAngles, DoorAnimTime)
+                .SetEase(Ease.Linear);
+        }
+
+        public override void SnapToOpenState()
+        {
+            base.SnapToOpenState();
+            DoorObject.transform.localRotation = Quaternion.Euler(_openedEulerAngles);
+        }
+        
+        public override void SnapToClosedState()
+        {
+            base.SnapToClosedState();
+            DoorObject.transform.localRotation = Quaternion.Euler(_closedEulerAngles);
+        }
+
+        protected override void Initialize()
+        {
+            _closedEulerAngles = DoorObject.transform.localRotation.eulerAngles;
+            _openedEulerAngles = _closedEulerAngles + Vector3.up * _doorOpenAngle;
+            base.Initialize();
+        }
+
+        public override string GetPromptSuffix()
+        {
+            return _objectName;
+        }
+
+        public override UniTask DoDisabledInteraction(CancellationToken token)
+        {
+            Debug.Log(this + " Door is disabled " , this);
+
+            return DoorObject.transform.DOShakePosition(DoorAnimTime*.5f, Vector3.one * _disabledDoorPunchAmount)
+                .SetEase(Ease.OutCirc)
+                .WithCancellation(token);
+        }
+    }
+}
---
+++ Samples/Demo/Scripts/TestItemPickup.cs
@@ -0,0 +1,59 @@
+using System.Threading;
+using Studio23.SS2.InteractionSystem.Abstract;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Studio23.SS2.InteractionSystem.Samples.Demo1
+{
+    public class TestItemPickup:ItemPickupBase
+    {
+        [SerializeField] private bool _canBeInterrupted = false;
+        [SerializeField] private string _pickUpName = "Shinpachi's shinpachis";
+
+        public override bool CanBeInterrupted => _canBeInterrupted;
+        public override string PickUpName => _pickUpName;
+        [SerializeField] private Sprite _markerIcon;
+        public override Sprite MarkerIcon => _markerIcon;
+
+        protected override void Initialize()
+        {
+            
+        }
+
+        protected override void HandleInteractionStarted()
+        {
+            
+        }
+
+        protected override void HandleInteractionPause()
+        {
+            
+        }
+
+        protected override void HandleInteractionResumed()
+        {
+            
+        }
+
+        public override void AddPickupToInventory()
+        {
+            base.AddPickupToInventory();
+            TestInventory.Instance.AddItem(this.PickUpName);
+        }
+
+        public override async UniTask DoNormalInteraction(CancellationToken token)
+        {
+            AddPickupToInventory();
+            
+            await UniTask.Yield();  
+        }
+
+        public override async UniTask  DoDisabledInteraction(CancellationToken token)
+        {
+            Debug.Log("can't add " + _pickUpName + " to inventory");
+            
+            await UniTask.Yield();  
+        }
+
+    }
+}
