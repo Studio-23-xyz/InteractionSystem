604f4be7229d87816e2e375bfdf6c1a5f93dcec6
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2024-06-05 20:28:28 +0600

    ## [v1.5.7]
    + Hover Icon is now for all classes that inherit interaction base and not Abstract for better control.
    + Hover Icon now moves with parent

---
+++ Samples/Demo/Editor/InteractionBillBoardAssigner.cs
@@ -0,0 +1,33 @@
+using Studio23.SS2.InteractionSystem.Abstract;
+using Studio23.SS2.InteractionSystem.UI;
+using UnityEditor;
+using UnityEngine;
+
+namespace Studio23.SS2.InteractionSystem.Editor
+{
+    // [CustomEditor(typeof(InteractableBase), true)]
+    // public class InteractionBillBoardAssigner : UnityEditor.Editor
+    // {
+    //     public override void OnInspectorGUI()
+    //     {
+    //         base.OnInspectorGUI();
+    //
+    //         InteractableBase interactable = (InteractableBase)target;
+    //
+    //         if (GUILayout.Button("Instantiate InteractionBillBoard"))
+    //         {
+    //             GameObject childPrefab = Resources.Load<GameObject>("InteractionSystem/InteractionBillBoard");
+    //             GameObject childObject = Instantiate(childPrefab);
+    //             childObject.transform.parent = interactable.transform;
+    //             childObject.transform.localPosition = Vector3.zero;
+    //             childObject.transform.localRotation = Quaternion.identity;
+    //
+    //             if (!interactable.TryGetComponent<InteractionMarkerController>(out var markerController))
+    //             {
+    //                 interactable.gameObject.AddComponent<InteractionMarkerController>();
+    //                 EditorUtility.SetDirty(interactable);
+    //             }
+    //         }
+    //     }
+    // }
+}
---
+++ Samples/Demo/Scripts/BasicInstantSwitch.cs
@@ -0,0 +1,48 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Studio23.SS2.InteractionSystem.Samples.Demo1
+{
+    public class BasicInstantSwitch:InstantSwitch
+    {
+        [SerializeField] Material _activeMateral;
+        [SerializeField] Material _inactiveMateral;
+        [SerializeField] private GameObject _matSwapTarget;
+        private Renderer _renderer;
+        [SerializeField] private Sprite _markerIcon;
+      
+        protected override void Initialize()
+        {
+            _renderer = _matSwapTarget.GetComponent<Renderer>();
+            base.Initialize();
+        }
+
+        public override UniTask DoDisabledInteraction(CancellationToken token)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override async UniTask DoActivateInteraction(CancellationToken cancellationToken)
+        {
+            await base.DoActivateInteraction(cancellationToken);
+            _renderer.sharedMaterial = _activeMateral;
+        }
+
+        protected override async UniTask DoDeactivateInteraction(CancellationToken cancellationToken)
+        {
+            await base.DoDeactivateInteraction(cancellationToken);
+            _renderer.sharedMaterial = _inactiveMateral;
+        }
+
+        public override void SnapToActivatedState()
+        {
+            _renderer.sharedMaterial = _activeMateral;
+        }
+
+        public override void SnapToDeactivatedState()
+        {
+            _renderer.sharedMaterial = _inactiveMateral;
+        }
+    }
+}
