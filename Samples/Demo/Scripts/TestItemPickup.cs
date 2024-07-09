8de1bcda7c8714df09004583d3f1811d4d7f16a8
B-DeshiDev <ffalsoy@gmail.com>
2024-07-09 10:26:49 +0600

    Add interactable parameter to events

---
+++ Samples/Demo/Scripts/TestItemPickup.cs
@@ -0,0 +1,51 @@
+using System.Threading;
+using Studio23.SS2.InteractionSystem.Abstract;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Studio23.SS2.InteractionSystem.Samples.Demo1
+{
+    public class TestItemPickup:ItemPickupBase
+    {
+        [SerializeField] private bool _canBeInterrupted = false;
+        [SerializeField] private string _pickUpName = "Shinpachi's shinpachis";
+
+        public override bool CanBeInterrupted => _canBeInterrupted;
+        public override string PickUpName => _pickUpName;
+
+
+        protected override void Initialize()
+        {
+            
+        }
+
+        protected override void HandleInteractionStarted()
+        {
+            
+        }
+
+        protected override void HandleInteractionPause()
+        {
+            
+        }
+
+        protected override void HandleInteractionResumed()
+        {
+            
+        }
+
+        public override void AddPickupToInventory()
+        {
+            base.AddPickupToInventory();
+            TestInventory.Instance.AddItem(this.PickUpName);
+        }
+
+        public override async UniTask DoInteraction(CancellationToken token)
+        {
+            AddPickupToInventory();
+            
+            await UniTask.Yield();  
+        }
+
+    }
+}
---
+++ Samples/Demo/Scripts/TestPlayerController.cs
@@ -0,0 +1,64 @@
+using Studio23.SS2.InteractionSystem.Abstract;
+using Studio23.SS2.InteractionSystem.Core;
+using Studio23.SS2.InteractionSystem.Samples.Demo1;
+using UnityEngine;
+
+namespace Studio23.SS2.InteractionSystem.Samples.Demo1.Scripts
+{
+    public class TestPlayerController:MonoBehaviour
+    {
+        private PlayerInteractionFinder _interactionFinder;
+        private TestCharacterController _controller;
+        [SerializeField] private bool _isPaused = false;
+
+        private void HandleInteractionStart(InteractableBase interactableBase)
+        {
+            _isPaused = true;
+            _controller.Toggle(false);
+        }
+
+        private void HandleInteractionEnded(InteractableBase interactableBase)
+        {
+            _isPaused = false;
+            _controller.Toggle(true);
+        }
+        
+        private void LookForInteractables()
+        {
+            var interactables = _interactionFinder.FindInteractables();
+            InteractionManager.Instance.ShowNewInteractables(interactables);
+        }
+
+        private void Update()
+        {
+            if (_isPaused)
+            {
+                return;
+            }
+
+            LookForInteractables();
+        }
+
+        private void Awake()
+        {
+            _interactionFinder = GetComponent<PlayerInteractionFinder>();
+            _controller = GetComponent<TestCharacterController>();
+        }
+        
+        private void Start()
+        {
+            InteractionManager.Instance.OnInteractionChainStarted += HandleInteractionStart;
+            InteractionManager.Instance.OnInteractionChainEnded += HandleInteractionEnded;
+        }
+
+        private void OnDisable()
+        {
+            if (InteractionManager.Instance != null)
+            {
+                InteractionManager.Instance.OnInteractionChainStarted -= HandleInteractionStart;
+                InteractionManager.Instance.OnInteractionChainEnded -= HandleInteractionEnded;
+            }
+        }
+    }
+    
+}
