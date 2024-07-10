55cd02f655b4e4cbd0d818186994f099a53d1374
B-DeshiDev <ffalsoy@gmail.com>
2024-07-10 17:12:11 +0600

    Allow toggling cam stacking from inspectable

---
+++ Runtime/UI/InteractButtonPromptView.cs
@@ -0,0 +1,35 @@
+using Studio23.SS2.InteractionSystem.Abstract;
+using Studio23.SS2.InteractionSystem.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Studio23.SS2.InteractionSystem.UI
+{
+    public class InteractButtonPromptView:InteractButtonPromptViewBase
+    {
+        [SerializeField] TextMeshProUGUI _promptTMP;
+        [SerializeField] private Slider _progressSlider;
+        [SerializeField] private GameObject _disabledIndicator;
+        
+        public override void showInteractable(InteractableBase interactableBase)
+        {
+            CurInteractable = interactableBase;
+            if (interactableBase != null)
+            {            
+                _promptTMP.text = interactableBase.GetPromptPrefix() + ":"+ interactableBase.GetPromptSuffix();
+                _disabledIndicator.gameObject.SetActive(interactableBase.LastEvaluationResult != InteractionConditionResult.Show);
+            }
+        }
+        
+        public override void ToggleProgressSlider(bool shouldShowSlider)
+        {
+            _progressSlider.gameObject.SetActive(shouldShowSlider);
+        }
+
+        public override void SetProgress(float progress)
+        {
+            _progressSlider.value = progress;
+        }
+    }
+}
