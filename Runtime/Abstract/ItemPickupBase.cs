86035a3a80fdcbb0fee71c2da29e47c17288b3ec
B-DeshiDev <ffalsoy@gmail.com>
2024-09-11 14:38:58 +0600

    Update package.json

---
+++ Runtime/Abstract/ItemPickupBase.cs
@@ -0,0 +1,25 @@
+using Bdeshi.Helpers.Input;
+using Studio23.SS2.InteractionSystem.Core;
+using UnityEngine;
+
+namespace Studio23.SS2.InteractionSystem.Abstract
+{
+    public abstract class ItemPickupBase : InteractableBase
+    {
+        public abstract string PickUpName { get; }
+
+        protected override void HandleInteractionCompleted()
+        {
+              // gameObject.SetActive(false);
+              Destroy(gameObject);
+        }
+
+        
+        public virtual void AddPickupToInventory()
+        {
+            Debug.Log("item added to inventory" + this, this);
+        }
+
+        public override InputButtonSlot InputButton => InteractionManager.Instance.InputHandler.PickupButton;
+    }
+}
